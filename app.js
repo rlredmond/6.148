be33112fec513f5030a95b105066a4c44595c439
Robert Redmond <rlredmond@gmail.com>
2018-01-10 14:36:29 -0500

    setting up local host for app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hellowolrd");
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+	res.end("AHHHHHHHHH");
+});
+
+const port = 3000;
+server.listen(port, function() {
+	console.log("Server running on port: " + port);
+});
