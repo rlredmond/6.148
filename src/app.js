5b223ebc9347d3232a1989151969186cf9f511c4
Robert Redmond <rlredmond@gmail.com>
2018-01-10 18:56:47 -0500

    Express functionality in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const http = require('http');
+const express = require("express");
 
-const server = http.createServer(function(req, res) {
-	res.end("AHHHHHHHHH");
+const app = express();
+
+app.get("/", function(req,res) {
+	res.send("bah")
 });
 
+
 const port = 3000;
+const server = http.Server(app);
 server.listen(port, function() {
 	console.log("Server running on port: " + port);
 });
